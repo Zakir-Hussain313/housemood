42de0e038698aeef0bd92e389865d1d0859e3f7a
Zakir-Hussain313 <regalt0s375@gmail.com>
2025-12-16 15:55:34 +0500

    Update favicon, metadata, SEO, etc.

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://housemood-beige.vercel.app/',
+  generateRobotsTxt: true, // automatically generates robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+};
